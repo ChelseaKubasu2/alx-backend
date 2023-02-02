1ea10176997d2672310df18204b654ffcfd1a736
ChelseaKubasu2 <kubasuchelsea@gmail.com>
2023-02-02 07:27:49 -0800

    add files

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+""" Flask app module
+"""
+
+from flask import Flask, render_template, request, flash
+from flask_babel import Babel, _
+from typing import Optional
+import gettext
+import os
+
+app = Flask(__name__)
+babel = Babel(app)
+
+app.secret_key = os.environ.get("FLASK_SECRET_KEY", "ABCDEF")
+
+
+class Config:
+    """ Configuration of available languages """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale() -> Optional[str]:
+    """ Return best match from accepted languages """
+    if request.args.get("locale") in app.config['LANGUAGES']:
+        return request.args.get("locale")
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def home() -> str:
+    """ Home page """
+    flash(_('home_title'))
+    flash(_('home_header'))
+    return render_template("4-index.html")
+
+
+# @app.route("/error")
+# def error():
+#     raise Exception("Error!")
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000)
